23135a7b414fef68bc75e6c677e6d68f2364ad5c
acidpore <acicitforezt@gmail.com>
2026-03-25 17:05:54 +0700

    Initial Setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express!' })
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
